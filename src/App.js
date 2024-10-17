692193886a43922b92a47d32e5593e1784efe36f
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-16 19:18:53 -0500

    Menu bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import Header from './components/Header';
 import CompanyList from './components/CompanyList';
 import CompanyDetail from './components/CompanyDetail';
 import Search from './components/Search';
 import Login from './components/Login';
 import Register from './components/Register';
-import Profile from './components/Profile';
-import Settings from './components/Settings';
 import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-100">
-          <Header />
-          <main className="container mx-auto py-8 pb-16">
-            <Routes>
-              <Route path="/" element={<CompanyList />} />
-              <Route path="/company/:id" element={<CompanyDetail />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </main>
-          <MenuBar />
-        </div>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <main className="container mx-auto py-8">
+          <Routes>
+            <Route path="/" element={<CompanyList />} />
+            <Route path="/company/:id" element={<CompanyDetail />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <MenuBar />
+      </div>
+    </Router>
   );
 }
 
